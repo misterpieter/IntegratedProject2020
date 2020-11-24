e8be3d2493157fdcc39d28a63fc1cb9c5cac4f7b
Pieter <volders.pieter@gmail.com>
2020-11-24 18:01:18 +0100

    basis pagina met knoppen voor functionaliteiten

--- app/src/main/java/be/volders/integratedproject2020/MainActivity.kt
+++ app/src/main/java/be/volders/integratedproject2020/MainActivity.kt
@@ -3,6 +3,7 @@ package be.volders.integratedproject2020
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import be.volders.integratedproject2020.Signature.SignatureActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +15,25 @@ class MainActivity : AppCompatActivity() {
         btnSignature.setOnClickListener {
             intent = Intent(this, SignatureActivity::class.java)
             startActivity(intent)
+        }
+
+        btnLogin.setOnClickListener {
+            Toast.makeText(this, "Nog niet geimplementeerd", Toast.LENGTH_SHORT).show()
+        }
+
+        btnCoordinates.setOnClickListener {
+            Toast.makeText(this, "Nog niet geimplementeerd", Toast.LENGTH_SHORT).show()
+        }
+
+        btnExportCsv.setOnClickListener {
+            Toast.makeText(this, "Nog niet geimplementeerd", Toast.LENGTH_SHORT).show()
+        }
+
+        btnImportCSV.setOnClickListener {
+            Toast.makeText(this, "Nog niet geimplementeerd", Toast.LENGTH_SHORT).show()
+        }
+        btnImportCSV.setOnClickListener {
+            Toast.makeText(this, "Nog niet geimplementeerd", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/be/volders/integratedproject2020/Signature/SignatureActivity.kt
+++ app/src/main/java/be/volders/integratedproject2020/Signature/SignatureActivity.kt
@@ -1,11 +1,21 @@
 package be.volders.integratedproject2020.Signature
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Bitmap.CompressFormat
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import be.volders.integratedproject2020.DrawingView
 import be.volders.integratedproject2020.R
 import be.volders.integratedproject2020.StrokeManager
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
 
 lateinit var recognize:Button
 lateinit var clear:Button
@@ -21,7 +31,9 @@ class SignatureActivity : AppCompatActivity() {
         clear = findViewById(R.id.clear)
         drawingView = findViewById(R.id.drawingView)
 
+
         recognize.setOnClickListener {
+
             StrokeManager.recognize(this)
         }
 
