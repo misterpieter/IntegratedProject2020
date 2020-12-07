a8d61fab65e263a85a3e7846db6838aab37eb9b1
Adriaenssens Jonas <jonas.adriaenssens@gmail.com>
2020-12-07 20:14:55 +0100

    fix StudentListActivity.kt

--- app/src/main/java/be/volders/integratedproject2020/Students/StudentListActivity.kt
+++ app/src/main/java/be/volders/integratedproject2020/Students/StudentListActivity.kt
@@ -23,7 +23,7 @@ lateinit var rvPersons: RecyclerView
 lateinit var sortlist: List<Student>
 lateinit var studentlist : ArrayList<Student>
 
-abstract class StudentListActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
+class StudentListActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     var databaseHelper: DatabaseHelpe? = DatabaseHelpe(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,58 +36,66 @@ abstract class StudentListActivity : AppCompatActivity(), AdapterView.OnItemSele
         studentlist = databaseHelper!!.getAllStudent()
         //Log.d("TAG", "databaseHelper list of student: ${studentlist}")
         val spinner: Spinner = findViewById(R.id.spSorteren)
-        val adapterSort = ArrayAdapter.createFromResource(this, R.array.sort_list, android.R.layout.simple_spinner_item)
+        val adapterSort = ArrayAdapter.createFromResource(
+            this,
+            R.array.sort_list,
+            android.R.layout.simple_spinner_item
+        )
 
         adapterSort.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapterSort
         spinner.setOnItemSelectedListener(this)
 
         btfilter = findViewById(R.id.btFilter)
-        btfilter?.setOnClickListener{
+      /*  btfilter?.setOnClickListener {
             etFilter = ettFilter.text.toString()
-            sortlist=  studentlist.filter { x -> x.name == etFilter || x.lastname == etFilter || x.snumber == etFilter }
+            sortlist =
+                studentlist.filter { x -> x.name == etFilter || x.lastname == etFilter || x.snumber == etFilter }
             //Log.d("LIST", "databaseHelper list of student: ${sortlist}")
             val adapter = StudentAdapter(this, sortlist)
             rvPersons.adapter = adapter
-        }
+        }*/
 
-        //databaseHelper!!.filterStudent()
+       // databaseHelper!!.filterStudent()
         //val studentList = getStudentsFromLocalCSV(this)
     }
 
-/*    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         var etFilter = ettFilter.text.toString()
         when (position) {
             0 -> {
-                sortlist =  databaseHelper!!.filterStudent("firstname")
+                sortlist= studentlist.sortedWith(compareBy { it.lastname })
                 val adapter = StudentAdapter(this, sortlist)
                 rvPersons.adapter = adapter
 
             }
             1 -> { //lastname
-                sortlist= studentlist.sortedWith(compareBy({it.lastname}))
+                sortlist = studentlist.sortedWith(compareBy({ it.lastname }))
                 val adapter = StudentAdapter(this, sortlist)
                 rvPersons.adapter = adapter
             }
             2 -> { //student_id
-                sortlist= studentlist.sortedWith(compareBy({it.snumber}))
+                sortlist = studentlist.sortedWith(compareBy({ it.snumber }))
                 val adapter = StudentAdapter(this, sortlist)
                 rvPersons.adapter = adapter
             }
             3 -> { //locationTime
-                sortlist= studentlist.sortedWith(compareBy({it.snumber}))
+                sortlist = studentlist.sortedWith(compareBy({ it.snumber }))
                 val adapter = StudentAdapter(this, sortlist)
                 rvPersons.adapter = adapter
             }
         }
-    }*/
+    }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
         val adapter = StudentAdapter(this, studentlist)
         rvPersons.adapter = adapter
     }
-    fun filterList(list: ArrayList<Student>, filter: String): Optional<Student>{
-        var listt = list.stream().filter { x -> x.name == filter || x.lastname == filter || x.snumber == filter }.findAny()
+
+    fun filterList(list: ArrayList<Student>, filter: String): Optional<Student> {
+        var listt = list.stream()
+            .filter { x -> x.name == filter || x.lastname == filter || x.snumber == filter }
+            .findAny()
         return listt
     }
 
