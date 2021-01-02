3f2346d9a568c02d27b9b9f6d68a02c3bd66f7cb
Volders Pieter (s054971) <pieter.volders@student.ap.be>
2021-01-02 15:28:25 +0100

    passwoord visibiliteit en button aangepast voor admin/student

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ Integrated Project 2020
 
 TO DO:
 
-Paswoord weg bij student: PIETER
 Signature risico JONAS
 PAD LAYOUT aanpassen: PIETER
 Bij importeren csv: automatisch lokaal opslaan: HALIMA en PIETER
@@ -13,30 +12,3 @@ BUG :
 detail Listview student heeft duplicaten
 Obama crash oplossen: JONAS
 Crash lat en Lon bij afwezigheid van wifi/mobile oplossen: JONAS
-
-
-
-
-
-
-
-SAMENVATTING
-voormalig “I was there” (app om een student in te loggen met handtekening)
-
-2 DELEN
-
-USER gedeelte
-inbreng handtekening (enkel als de student in de lijst staat, opgesteld door de admin)
-GEEN feedback van suspicion level; enkel handtekening zetten -> popup dat het is gelukt; de rest gebeurd achter de schermen
-
-
-ADMIN gedeelte
-- Mogelijkheid tot CRUD van studenten. lijst van student met relevante gegevens
-	Enkel wanneer de student in de lijst staat, heeft deze de mogelijkheid om in te loggen met zijn handtekening
-	Belangrijk dat de admin deze lijst makkelijk kan exporteren (via CSV of iets anders: XLXS/JSON?) 
-- overzicht handtekeningen met naam, studentennr, datum en locatie (reverse address lookup)
-- voorzien van zoek/filter mogelijkheden
-- beschermd door pwd 
-- Data wordt lokaal weggeschreven maar kan gesynchroniseerd worden in firebase via synchronisatie knop
-- bonuspunten voor handtekening verificatie, liefst lokaal en onmiddellijk
-	Dit gebeurd dan via een “suspicion level” (omdat het moeilijk te achterhalen is of een digitale handtekening EFFECTIEF niet door de juiste persoon is gezet)
--- app/src/main/java/be/volders/integratedproject2020/MainActivity.kt
+++ app/src/main/java/be/volders/integratedproject2020/MainActivity.kt
@@ -10,6 +10,7 @@ import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import be.volders.integratedproject2020.Admin.AdminActivity
 import be.volders.integratedproject2020.Helper.getStudentsFromLocalCSV
 import be.volders.integratedproject2020.Model.Address
@@ -23,9 +24,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adres : Address
     private lateinit var selectedStudent:Student
 
+    private val ADMIN_NAME = "Admin"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // LOGIN
+        btnLogin.isEnabled = false
+        etPassword.isVisible = false
+
+
         // lijst hardcoded van studenten
         val studentList = ArrayList<Student>()
         studentList.add(Student( "Admin","Admin","pnumber","admin"))
@@ -44,19 +53,27 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, studentList)
         actvStudents.setAdapter(adapter)
+
         // event click listener op zoekbalk van studenten
         actvStudents.setOnItemClickListener { parent, view, position, id ->
             selectedStudent = parent.getItemAtPosition(position) as Student
             Helper.hideKeyboard(parentView!!,this)
+
+            var boolAdmin = selectedStudent.name == ADMIN_NAME
+
+            if (!boolAdmin) {
+                etPassword.isVisible = false
+                btnLogin.isEnabled = true
+            }
+            else etPassword.isVisible = true
         }
 
-        // LOGIN
-        //btnLogin.isEnabled = false
+
 
         btnLogin?.setOnClickListener {
             var password:String = etPassword.text.toString()
             var studentname = selectedStudent.name
-            var boolAdmin = studentname == "Admin"
+            var boolAdmin = studentname == ADMIN_NAME
             //not admin
             if (!boolAdmin ){
                 intent = Intent(this, SignatureActivity::class.java)
@@ -66,22 +83,21 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
 
                 resetPage()
-                Toast.makeText(this, "STUDENT", Toast.LENGTH_SHORT).show()
+//                Toast.makeText(this, "STUDENT", Toast.LENGTH_SHORT).show()
             }
             else if (boolAdmin && selectedStudent.password == password) {
                 intent = Intent(this, AdminActivity::class.java)
                 startActivity(intent)
                 resetPage()
-                Toast.makeText(this, "ADMIN", Toast.LENGTH_SHORT).show()
+//                Toast.makeText(this, "ADMIN", Toast.LENGTH_SHORT).show()
             }
             else {
                 Toast.makeText(this, "FOUTE INPUT!", Toast.LENGTH_SHORT).show()
             }
         }
 
-        // DEV  => visibility in comment zetten
 
-        //etPassword.addTextChangedListener(textWatcher)
+        etPassword.addTextChangedListener(textWatcher)
 
         btnAdmin.setOnClickListener {
             intent = Intent(this, AdminActivity::class.java)
@@ -93,9 +109,8 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
     }
-/*
+
     private val textWatcher = object : TextWatcher {
 
         override fun afterTextChanged(s: Editable?) {
@@ -106,7 +121,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            isPasswordEmpty(s.toString())
+            isPasswordEmpty(s.toString(),5)
         }
     }
 
@@ -114,12 +129,13 @@ class MainActivity : AppCompatActivity() {
         var length = string.trim().length
         btnLogin.isEnabled = length >= minimumPasswordLength
     }
-*/
+
     private fun resetPage(){
         btnLogin.isEnabled = false
         actvStudents.setText("")
         etPassword.setText("")
     }
+
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         if (requestCode == locationPermissionCode) {
